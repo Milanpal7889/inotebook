0d6439897f57bff4b5de77a4fc75c7fef0fe25ac
Milanpal7889 <milanpaul212@gmail.com>
2023-08-05 08:07:00 +0530

    router working

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
         <nav className = "relative flex w-full flex-nowrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:flex-wrap lg:justify-start lg:py-4" data-te-navbar-ref>
             <div className = "flex w-full flex-wrap items-center justify-between px-3">
                 <div className = "ml-2">
-                    <Link className = "text-xl text-neutral-800 dark:text-neutral-200" href = "/">
+                    <Link className = "text-xl text-neutral-800 dark:text-neutral-200" to = "/">
                         Navbar
                     </Link>
                 </div>
@@ -21,22 +21,22 @@ function Navbar() {
                 <div className = "!visible mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto" id="navbarSupportedContent2" data-te-collapse-item>
                     <ul className = "list-style-none mr-auto flex flex-col pl-0 lg:mt-1 lg:flex-row" data-te-navbar-nav-ref>
                         <li className = "my-4 pl-2 lg:my-0 lg:pl-2 lg:pr-1" data-te-nav-item-ref>
-                            <Link className = "active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" aria-current="page" href="/" data-te-nav-link-ref >
+                            <Link className = "active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" aria-current="page" to="/about" data-te-nav-link-ref >
                                 Home
                             </Link>
                         </li>
                         <li className = "mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1" data-te-nav-item-ref>
-                            <Link className="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" href="/" data-te-nav-link-ref >
+                            <Link className="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" to="/" data-te-nav-link-ref >
                                 Features
                             </Link>
                         </li>
                         <li className="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1" data-te-nav-item-ref>
-                            <Link className="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" href="/" data-te-nav-link-ref>
+                            <Link className="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" to="/" data-te-nav-link-ref>
                                 Pricing
                             </Link>
                         </li>
                         <li className="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1" data-te-nav-link-ref>
-                            <Link href="/" className="text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400">
+                            <Link to="/" className="text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400">
                                 Disabled
                             </Link>
                         </li>
